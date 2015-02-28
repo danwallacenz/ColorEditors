25c9fee76fd1d1c3575fc0794e6e27a2efbd06ce
Daniel Wallace <daniel.wallace.nz@gmail.com>
2015-02-28 16:16:25 +1300

    add ColorEditorView

---
+++ ColorEditors/ColorEditorView.swift
@@ -0,0 +1,21 @@
+//
+//  ColorEditorView.swift
+//  ColorEditors
+//
+//  Created by Daniel Wallace on 28/02/15.
+//  Copyright (c) 2015 nz.co.danielw. All rights reserved.
+//
+
+import UIKit
+
+class ColorEditorView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
